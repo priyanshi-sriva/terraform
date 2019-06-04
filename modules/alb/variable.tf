515255e42554e7ea03cc7deb2a39a75cc674ee9a
priyanshi-sriva <priyanshi.srivastava@tothenew.com>
2019-06-04 17:16:50 +0530

    updated modules

--- modules/alb/variable.tf
+++
@@ -1,6 +0,0 @@
-variable "vpc"{}
-
-variable "subnet_id_1"{}
-variable "subnet_id_2"{}
-variable "instance-id"{
-}
--- modules/ec2/output.tf
+++
@@ -1,9 +0,0 @@
-output "security_group"{
-
-value = "${aws_security_group.security_group.id}"
-}
-
-output "instanceid"{
-
-value = "${aws_instance.ec2.id}"
-}
